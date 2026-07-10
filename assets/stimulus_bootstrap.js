ab6151b42d1e56c00a9de67af7e1e591dc223749
AntarAlbert <136308728+AntarAlbert@users.noreply.github.com>
2026-07-10 22:37:36 +0300

    Fix: Enregistrer les contrôleurs Stimulus csrf-protection et otp

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,5 +1,8 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
+import CsrfProtectionController from './controllers/csrf_protection_controller.js';
+import OtpController from './controllers/otp_controller.js';
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+app.register('csrf-protection', CsrfProtectionController);
+app.register('otp', OtpController);
